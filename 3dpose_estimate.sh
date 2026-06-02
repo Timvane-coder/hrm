9da08b5969ca04045f395e0f553fe67292dcd2ed
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2026-06-02 15:38:50 +0200

    Update 3dpose_estimate.sh

--- 3dpose_estimate.sh
+++ 3dpose_estimate.sh
@@ -17,7 +17,7 @@ for f in keras_Realtime_Multi-Person_Pose_Estimation/sample_images/*; do
     python hmr/3dpose_estimate.py \
         --img_path  "$f" \
         --json_path "keras_Realtime_Multi-Person_Pose_Estimation/sample_jsons/${no_ext}.json" \
-        --out_dir   "hmr/output/csv" \
+        --out_dir   "output/csv" \
         --frame     "$FRAME"
 
     FRAME=$((FRAME + 1))
